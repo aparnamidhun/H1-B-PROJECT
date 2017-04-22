9435833fd32d887baaa2755dba4191bd67f25832
Aparna v s <aparnavv.22@gmail.com>
2017-04-23 01:30:55 +0530

    H1-B-proect activities
    This case is analysis by using 'HIVE' queries

---
+++ H1-B-PROECT-2(A).sql
@@ -0,0 +1,16 @@
+---------------HIVE--------------
+
+
+--2 a) Which part of the US has the most Data Engineer jobs for each year?
+
+SELECT worksite,COUNT(case_status) AS number_of_petition,year from h1b_applications WHERE job_title like '%DATA ENGINEER%' and year='2011' GROUP BY worksite,year ORDER BY number_of_petition desc limit 1;
+-worksite	number_of_petition	year
+-SEATTLE, WASHINGTON	20	2011
+
+SELECT worksite,COUNT(case_status) AS number_of_petition,year from h1b_applications WHERE job_title='DATA ENGINEER' and year='2012' GROUP BY worksite,year ORDER BY number_of_petition desc limit 1;
+--worksite	number_of_petition	year
+SAN FRANCISCO, CALIFORNIA	7	2012
+
+SELECT worksite,COUNT(case_status) AS number_of_petition,year from h1b_applications WHERE job_title='DATA ENGINEER' and year='2013' GROUP BY worksite,year ORDER BY number_of_petition desc limit 1;
+SELECT worksite,COUNT(case_status) AS number_of_petition,year from h1b_applications WHERE job_title='DATA ENGINEER' and year='2014' GROUP BY worksite,year ORDER BY number_of_petition desc limit 1;
+SELECT worksite,COUNT(case_status) AS number_of_petition,year from h1b_applications WHERE job_title='DATA ENGINEER' and year='2015' GROUP BY worksite,year ORDER BY number_of_petition desc limit 1;
