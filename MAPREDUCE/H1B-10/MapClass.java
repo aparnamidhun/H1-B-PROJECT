8202ea941f04776a38d34429fe62ec46478e651d
Aparna v s <aparnavv.22@gmail.com>
2017-04-30 10:36:44 +0530

    Update MapClass.java

--- MAPREDUCE/H1B-10/MapClass.java
+++ MAPREDUCE/H1B-10/MapClass.java
@@ -9,7 +9,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 		public void map(LongWritable key,Text value,Context context) throws IOException, InterruptedException{
 			String[] parts = value.toString().split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");//From statckoverflow.com
 			String status = parts[1];
-			String job = parts[4];
+			String job = parts[4].replaceAll("\"","");
 			context.write(new Text(job), new Text(status));
 		}
 	}
